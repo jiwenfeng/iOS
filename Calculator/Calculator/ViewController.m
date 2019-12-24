384607a6dff21089c88206dd5ad140e1905813f7
jiwenfeng <490257094@qq.com>
2019-12-24 22:13:46 +0800

    添加try catch捕获异常

--- Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/ViewController.m
@@ -52,76 +52,103 @@
 
 -(void) onSignButtonClick:(UIButton *)button
 {
-    display.text = [[NSString alloc]initWithFormat:@"%g", ([display.text doubleValue] * -1)];
+    @try {
+        display.text = [[NSString alloc]initWithFormat:@"%g", ([display.text doubleValue] * -1)];
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception);
+    } @finally {
+    }
 }
 
 -(void) onPercentButtonClick:(UIButton *)button
 {
-    display.text = [[NSString alloc]initWithFormat:@"%g", [display.text doubleValue] / 100];
+    @try {
+        display.text = [[NSString alloc]initWithFormat:@"%g", [display.text doubleValue] / 100];
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception);
+    } @finally {
+    }
 }
 
 -(void) onNumberButtonClick:(UIButton *)button
 {
-    if([display.text isEqualToString:@"0"])
-    {
-        display.text = button.titleLabel.text;
-    }
-    else if ([display.text isEqualToString:@"-0"])
-    {
-        display.text = [[[NSString alloc] initWithString:@"-"] stringByAppendingString:button.titleLabel.text];
-    }
-    else
-    {
-        if (clear)
+    @try {
+        if([display.text isEqualToString:@"0"])
         {
             display.text = button.titleLabel.text;
-            clear = FALSE;
+        }
+        else if ([display.text isEqualToString:@"-0"])
+        {
+            display.text = [[[NSString alloc] initWithString:@"-"] stringByAppendingString:button.titleLabel.text];
         }
         else
         {
-            display.text = [[[NSString alloc] initWithString:display.text] stringByAppendingString:button.titleLabel.text];
+            if (clear)
+            {
+                display.text = button.titleLabel.text;
+                clear = FALSE;
+            }
+            else
+            {
+                display.text = [[[NSString alloc] initWithString:display.text] stringByAppendingString:button.titleLabel.text];
+            }
         }
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception);
+    } @finally {
     }
 }
 
 -(void) onOpButtonClick:(UIButton *)button
 {
-    if(isnan(omitted_operand))
-    {
-        omitted_operand = [display.text doubleValue];
-    }
-    else if(isnan(operand))
-    {
-        operand = [display.text doubleValue];
-    }
-
-    if ([button.titleLabel.text isEqualToString:@"="])
-    {
-        omitted_operand = [self doCalculate:omitted_operand:lastop :operand];
-    }
-    else
-    {
-        if(lastop == [button.titleLabel.text characterAtIndex:0])
+    @try {
+        
+        if(isnan(omitted_operand))
         {
-            omitted_operand = [self doCalculate:omitted_operand :lastop :operand];
+            omitted_operand = [display.text doubleValue];
+        }
+        else if(isnan(operand))
+        {
+            operand = [display.text doubleValue];
+        }
+        
+        if ([button.titleLabel.text isEqualToString:@"="])
+        {
+            omitted_operand = [self doCalculate:omitted_operand:lastop :operand];
         }
         else
         {
-            operand = NAN;
+            if(lastop == [button.titleLabel.text characterAtIndex:0])
+            {
+                omitted_operand = [self doCalculate:omitted_operand :lastop :operand];
+            }
+            else
+            {
+                operand = NAN;
+            }
+            lastop = [button.titleLabel.text characterAtIndex:0];
         }
-        lastop = [button.titleLabel.text characterAtIndex:0];
+        display.text = [[NSString alloc] initWithFormat:@"%g", omitted_operand];
+        clear = TRUE;
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception) ;
+    } @finally {
     }
-    display.text = [[NSString alloc] initWithFormat:@"%g", omitted_operand];
-    clear = TRUE;
 }
 
 -(void) onPointButtonClick:(UIButton *)button
 {
-    if ([display.text rangeOfString:@"."].location != NSNotFound)
-    {
-        return;
+    @try {
+        
+        if ([display.text rangeOfString:@"."].location != NSNotFound)
+        {
+            return;
+        }
+        display.text = [[[NSString alloc] initWithString:display.text] stringByAppendingString:@"."];
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception);
+    } @finally {
     }
-    display.text = [[[NSString alloc] initWithString:display.text] stringByAppendingString:@"."];
 }
 
 -(NSString *)getButtonText:(int)line :(int)column
@@ -138,92 +165,97 @@
 
 -(void)createButton:(int)line :(int)column
 {
-    if(line == 4 && column == 3)
-    {
-        return;
-    }
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-
-    [btn setTitle:[self getButtonText:line:column] forState:UIControlStateNormal];
-    if (line == 4 && column == 0)
-    {
-        btn.frame = CGRectMake(10 + 100 * column , 300 + 100 * line, 180, 90);
-    }
-    else
-    {
-        int n = column;
-        if (line == 4)
+    @try {
+        if(line == 4 && column == 3)
         {
-            n += 1;
+            return;
         }
-        btn.frame = CGRectMake(10 + 100 * n , 300 + 100 * line, 90, 90);
-    }
-    btn.layer.cornerRadius = 45;
-    if (line == 0 && column != 3)
-    {
-        btn.backgroundColor = [UIColor grayColor];
-    }
-    else
-    {
-        if (column == 3 || (line == 4 && column == 2))
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        
+        [btn setTitle:[self getButtonText:line:column] forState:UIControlStateNormal];
+        if (line == 4 && column == 0)
         {
-            btn.backgroundColor = [UIColor orangeColor];
+            btn.frame = CGRectMake(10 + 100 * column , 300 + 100 * line, 180, 90);
         }
         else
         {
-            btn.backgroundColor = [UIColor darkGrayColor];
+            int n = column;
+            if (line == 4)
+            {
+                n += 1;
+            }
+            btn.frame = CGRectMake(10 + 100 * n , 300 + 100 * line, 90, 90);
         }
-    }
-    
-    btn.layer.masksToBounds = YES;
-    btn.titleLabel.font = [UIFont systemFontOfSize:45];
-    
-    if (line == 0)
-    {
-        if (column == 0)
+        btn.layer.cornerRadius = 45;
+        if (line == 0 && column != 3)
         {
-            [btn addTarget:self action:@selector(onACButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-        if (column == 1)
-        {
-            [btn addTarget:self action:@selector(onSignButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-        if (column == 2)
-        {
-            [btn addTarget:self action:@selector(onPercentButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-        if (column == 3)
-        {
-            [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-    }
-    else if(line == 4)
-    {
-        if (column == 0)
-        {
-            [btn addTarget:self action:@selector(onNumberButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-        else if (column == 1)
-        {
-            [btn addTarget:self action:@selector(onPointButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-        else if(column == 2)
-        {
-            [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
-        }
-    }
-    else
-    {
-        if (column == 3)
-        {
-            [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            btn.backgroundColor = [UIColor grayColor];
         }
         else
         {
-            [btn addTarget:self action:@selector(onNumberButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            if (column == 3 || (line == 4 && column == 2))
+            {
+                btn.backgroundColor = [UIColor orangeColor];
+            }
+            else
+            {
+                btn.backgroundColor = [UIColor darkGrayColor];
+            }
         }
+        
+        btn.layer.masksToBounds = YES;
+        btn.titleLabel.font = [UIFont systemFontOfSize:45];
+        
+        if (line == 0)
+        {
+            if (column == 0)
+            {
+                [btn addTarget:self action:@selector(onACButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            if (column == 1)
+            {
+                [btn addTarget:self action:@selector(onSignButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            if (column == 2)
+            {
+                [btn addTarget:self action:@selector(onPercentButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            if (column == 3)
+            {
+                [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+        }
+        else if(line == 4)
+        {
+            if (column == 0)
+            {
+                [btn addTarget:self action:@selector(onNumberButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            else if (column == 1)
+            {
+                [btn addTarget:self action:@selector(onPointButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            else if(column == 2)
+            {
+                [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+        }
+        else
+        {
+            if (column == 3)
+            {
+                [btn addTarget:self action:@selector(onOpButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+            else
+            {
+                [btn addTarget:self action:@selector(onNumberButtonClick:) forControlEvents:(UIControlEventTouchUpInside)];
+            }
+        }
+        [self.view addSubview:btn];
+    } @catch (NSException *exception) {
+        NSLog(@"%@", exception);
+    } @finally {
     }
-    [self.view addSubview:btn];
 }
 
 -(void) doInit
